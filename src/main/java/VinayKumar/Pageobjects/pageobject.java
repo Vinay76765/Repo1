6e7c8806c44bc22230f7e92092a17bf4c84004cc
Vinay76765 <vinaykumar98.vk44@gmailcom>
2024-04-08 15:17:56 +0530

    final commit of master'

---
+++ src/main/java/VinayKumar/Pageobjects/pageobject.java
@@ -0,0 +1,20 @@
+package VinayKumar.Pageobjects;
+
+public class pageobject {
+	
+	public void class1()
+	{
+		System.out.println("Hii");
+	}
+	
+	public void class2()
+	{
+		System.out.println("Hello");
+	}
+	
+	public void class3()
+	{
+		System.out.println("Friendly");
+	}
+
+}
