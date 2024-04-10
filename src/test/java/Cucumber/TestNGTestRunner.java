3d4e883be5cc5c700dc91ee501015ae8b2577688
Vinay76765 <vinaykumar98.vk44@gmailcom>
2024-04-10 14:53:42 +0530

    10th commit

--- src/test/java/Cucumber/TestNGTestRunner.java
+++ src/test/java/Cucumber/TestNGTestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 
-@CucumberOptions(features="src/test/resources/CucumberFeatures/Create_course.feature", glue={"CucumberDefinitions","Hooks"},
+@CucumberOptions(features="src/test/resources/CucumberFeatures/Admin_subscription.feature", glue={"CucumberDefinitions","Hooks"},
 plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
 )
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
--- src/test/java/Cucumber/TestNGTestRunner_one.java
+++ src/test/java/Cucumber/TestNGTestRunner_one.java
@@ -3,7 +3,7 @@ package Cucumber;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features="src/test/resources/CucumberFeatures/Admin_subscription.feature", glue={"CucumberDefinitions","Hooks"},
+@CucumberOptions(features="src/test/resources/CucumberFeatures/Create_course.feature", glue={"CucumberDefinitions","Hooks"},
 monochrome=true)
 public class TestNGTestRunner_one extends AbstractTestNGCucumberTests {
 	
